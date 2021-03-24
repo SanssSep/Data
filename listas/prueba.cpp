0ab5ac09fe5b859a78870490dac563c7844e259d
Santiago Sepúlveda <santiago.sepulveda1@utp.edu.co>
2021-03-23 20:02:44 -0500

    Pruebo la funcion push_front

--- listas/prueba.cpp
+++ listas/prueba.cpp
@@ -2,13 +2,23 @@
 #include "list.hh"
 
 int main(){
+	//Defino un elemento de la clase List llamado prueba
 	List<int> prueba;	
-	cout << prueba.empty() << endl;
-	prueba.push_back(11);
-	prueba.push_back(88);
-	cout << prueba.empty();
+	//Agreglo los datos 10,7,9,3
+
+	cout << "Antes del push_front";
+	prueba.push_back(10);
+	prueba.push_back(7);
+	prueba.push_back(9);
+	prueba.push_back(3);
+	//Imprimo los datos 
 	prueba.print();
-	prueba.push_front(66);
+	cout << endl;
+	
+	//Agrego el elemento 1 al frente de la lista
+	prueba.push_front(1);
+	cout << "Despues del push_front";
+	//Imprimo los datos
 	prueba.print();
 	return 0;
 }
